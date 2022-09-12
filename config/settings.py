64558ecf7ab997a0a63d93c38bedd63a2fc3dcc5
Ruslan Alimov <rusichviz@gmail.com>
2022-09-12 16:07:19 +0300

    Changed settings and readme

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
